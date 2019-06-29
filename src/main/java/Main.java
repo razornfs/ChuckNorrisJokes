9fc2f3861f87d9c34901166e4e24ac5f046a6a68
Bonkers <kubaorz1@gmail.com>
2019-06-29 14:16:43 +0200

    never see the same joke twice!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,27 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         JokeSource source = new APIJokeSource();
-        source.getJokes(10).forEach(System.out::println);
+
+        Path path = Paths.get("jokes.txt");
+        List<String> strings = Files.readAllLines(path);
+
+        Set<String> oldJokes = new HashSet<>(strings);
+        Set<String> newJokes = new HashSet<>();
+        int jokeCount = 0;
+        while (jokeCount < 10) {
+            Joke joke = source.getJokes(1).get(0);
+            if (!oldJokes.contains(joke.getId())) {
+                newJokes.add(joke.getId());
+                System.out.println(joke.getValue());
+                jokeCount++;
+            }
+        }
+        Files.write(path, newJokes, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
     }
 }
